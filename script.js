ecc6ec34c388e804cf81c646e8795e6c68ef1c06
lav333 <val.dragasevic@gmail.com>
2024-09-25 13:04:26 +0300

    Add color change on hover and button for choosing square number

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+function changeBackgroundColor(element) {
+    element.style.backgroundColor = "darkgreen";
+}
+
+function changeSquareNumber() {
+    let newSquareNumber = prompt("Please type the wanted number between 1 and 100:");
+    createGrid(Number(newSquareNumber));
+}
+
+function createGrid(x) {
+    cont.replaceChildren()
+    if (typeof x == "number" && x>0 && x<101) {
+        for (let i=0; i<x; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            for (let j=0; j<x; j++) {
+                const square = document.createElement("div");
+                square.classList.add("square");
+                row.appendChild(square);
+                square.addEventListener("mousemove", () => changeBackgroundColor(square));
+            }
+            cont.appendChild(row);
+        }
+    }
+    else {
+        alert("Wrong input! Please try again!");
+        changeSquareNumber();
+    }
+}
+
 const cont = document.querySelector(".cont");
 
-for (let i=0; i<16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j=0; j<16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-    }
-    cont.appendChild(row);
-}
+createGrid(16);
+
+const squareNumber = document.querySelector(".squareNumber");
+squareNumber.addEventListener("click", () => changeSquareNumber());
